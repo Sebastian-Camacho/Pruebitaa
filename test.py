31b96aaaf2042ec566840126b45b075f28a2ec8b
Sebastian-Camacho <sebastian.camacho@coppel.com>
2023-07-27 09:41:29 -0700

    Se validó el nuevo fallo

--- test.py
+++ test.py
@@ -26,4 +26,10 @@ try:
 except Exception as e:
     print("El error es: ",e)
 
+try:
+    botonIngresar = driver.find_element(By.XPATH, "//button[contains(text(), 'Ingresar')]")
+    botonIngresar.click()
+except Exception as e:
+    print("Por si falla en donde sea ",e)
+
 driver.close()
