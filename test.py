eb3ece8f9e8ce5644bd2b59f4a4dc1cb17ebda16
Sebastian-Camacho <sebastian.camacho@coppel.com>
2023-07-27 09:42:52 -0700

    Se elimino el codigo duplicado

--- test.py
+++ test.py
@@ -24,12 +24,6 @@ try:
     botonIngresar = driver.find_element(By.XPATH, "//button[contains(text(), 'Ingresar')]")
     botonIngresar.click()
 except Exception as e:
-    print("El error es: ",e)
-
-try:
-    botonIngresar = driver.find_element(By.XPATH, "//button[contains(text(), 'Ingresar')]")
-    botonIngresar.click()
-except Exception as e:
     print("Por si falla en donde sea ",e)
 
 driver.close()
